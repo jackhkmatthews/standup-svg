37bac4d0cc258019d5e34de5e2d5946a4668df63
Jack Matthews <jackhkmatthews@gmail.com>
2017-07-07 10:03:30 +0100

    playing around with clearing of output folder pre new parse/render

--- app/index.js
+++ app/index.js
@@ -3,19 +3,33 @@ const jsdom     = require('jsdom');
 const d3        = require('d3');
 require('d3-selection-multi');
 const D3SvgDrawer = require('./drawer/d3-svg-drawer.js').D3SvgDrawer;
+var rimraf = require('rimraf');
 
 const data      = require('./temp/march-2017.json');
 
 const { JSDOM } = jsdom;
 
-data.forEach((day, index) => {
-  const d3SvgDrawer = new D3SvgDrawer(day);
+clearOutput();
 
-  JSDOM.fromFile(`${__dirname}/html/index.html`, { 
-    beforeParse(window) {
-      window.d3 = d3;
-    } 
-  }).then(dom => {
-    fs.writeFileSync(`${__dirname}/../output/svg/${index}.svg`, d3SvgDrawer.getSvgHtml(dom));
+// data.forEach((day, index) => {
+//   const d3SvgDrawer = new D3SvgDrawer(day);
+
+//   JSDOM.fromFile(`${__dirname}/html/index.html`, { 
+//     beforeParse(window) {
+//       window.d3 = d3;
+//     } 
+//   }).then(dom => {
+//     fs.writeFileSync(`${__dirname}/../output/svg/${index}.svg`, d3SvgDrawer.getSvgHtml(dom));
+//   });
+// });
+
+function clearOutput() {
+  rimraf(`${__dirname}/../output/svg`, () => {
+    const dir = `${__dirname}/../output/svg`;
+
+    if (!fs.existsSync(dir)){
+      fs.mkdirSync(dir);
+    }
+    console.log('done');
   });
-});
+}
