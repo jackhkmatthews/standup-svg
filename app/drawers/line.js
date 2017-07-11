55890853619c9ad9683143f8e069b22aca601d68
Jack Matthews <jackhkmatthews@gmail.com>
2017-07-11 10:34:33 +0100

    created d3 drawer for single line

--- app/drawers/line.js
+++ app/drawers/line.js
@@ -10,86 +10,48 @@ class D3SvgDrawer {
       .attr('version', 1.1)
       .attr('xmlns', 'http://www.w3.org/2000/svg')
       .attr('height', 200)
-      .attr('width', 200);
+      .attr('width', 200 );
 
-    const originX = 200;
-    const originY = 200;
+    const originX = 100;
+    const originY = 100;
     const outerCircleRadius = 100;
 
-    const outerCircle = svg.append('circle').attrs({
-      cx: originX,
-      cy: originY,
-      opacity: 0,
-      r: outerCircleRadius,
-      fill: 'none',
-      stroke: 'black'
-    });
-
-    let augmentedFormation = [];
-
-    this.day.formation.forEach((name, index, array) => {
-      const chairOriginX = originX + ((outerCircleRadius) * Math.sin(((2*Math.PI)/array.length)*index));
-      const chairOriginY = originY - ((outerCircleRadius) * Math.cos(((2*Math.PI)/array.length)*index));
-      const coordinate = [chairOriginX, chairOriginY];
+    const formation = this.day.formation.map((name, index, array) => {
+      const coodX = originX + ((outerCircleRadius) * Math.sin(((2*Math.PI)/array.length)*index));
+      const coodY = originY - ((outerCircleRadius) * Math.cos(((2*Math.PI)/array.length)*index));
+      const coordinate = [coodX, coodY];
       const person = {
         name,
         coordinate
       };
-      augmentedFormation.push(person);
-      let pointOnOuterCircle = svg.append('circle').attrs({
-        cx: chairOriginX,
-        cy: chairOriginY,
-        opacity: 1,
-        r: 5,
-        fill: 'black'
-      });
-      let nameOnOuterCircle = svg.append('text')
-        .attrs({
-          x: chairOriginX,
-          y: chairOriginY,
-          'font-family': 'sans-serif',
-          'font-size': '20px',
-          fill: 'black'
-        })
-        .text(name);
+      return person;
     });
 
-    const augmentedPasses = [];
+    let order = this.day.passes.reduce((acc, pass, index) => {
+      acc.push(pass.from);
+      if (index === this.day.passes.length -1) {
+        acc.push(pass.to);
+      }
+      return acc;
+    }, []);
 
-    this.day.passes.forEach((pass, index, array) => {
-      const augmentedPass = {
-        passIndex: pass.passIndex,
-        from: {
-          name: pass.from,
-          coordinate: []
-        },
-        to: {
-          name: pass.to,
-          coordinate: []
-        }
-      };
-      augmentedFormation.forEach((person, index, array) => {
-        if (person.name === pass.from) {
-          augmentedPass.from.coordinate = person.coordinate;
-        }
-        if (person.name === pass.to) {
-          augmentedPass.to.coordinate = person.coordinate;
-        }
-      });
-      augmentedPasses.push(augmentedPass);  
+    order = order.map(element => {
+      return formation.filter(person => {
+        return person.name === element;
+      })[0];
     });
+
 
     let lineGenerator = d3.line()
       .x(person => person.coordinate[0])
       .y(person => person.coordinate[1]);
 
-    augmentedPasses.forEach((augmentedPass, index, array) => {
-      let pathData = lineGenerator([augmentedPass.from, augmentedPass.to]);
-      let line = svg.append('path')
-        .attr('d', pathData)
-        .attr('stroke', 'black')
-        .attr('fill', 'transparent');
-    });
+    let pathData = lineGenerator(order);
+
+    let line = svg.append('path')
+      .attr('d', pathData)
+      .attr('stroke', 'black')
+      .attr('fill', 'transparent');
 
     return dom.window.document.querySelector('svg').outerHTML;
   }
--- app/drawers/test-environment/app.js
+++ app/drawers/test-environment/app.js
@@ -1,86 +1,46 @@
 const svg = d3.select(window.document).select('svg')
   .attr('version', 1.1)
   .attr('xmlns', 'http://www.w3.org/2000/svg')
-  .attr('height', 400)
-  .attr('width', 400);
+  .attr('height', 200)
+  .attr('width', 200 );
 
-const originX = 200;
-const originY = 200;
+const originX = 100;
+const originY = 100;
 const outerCircleRadius = 100;
 
-const outerCircle = svg.append('circle').attrs({
-  cx: originX,
-  cy: originY,
-  opacity: 0,
-  r: outerCircleRadius,
-  fill: 'none',
-  stroke: 'black'
-});
-
-let augmentedFormation = [];
-
-day.formation.forEach((name, index, array) => {
-  const chairOriginX = originX + ((outerCircleRadius) * Math.sin(((2*Math.PI)/array.length)*index));
-  const chairOriginY = originY - ((outerCircleRadius) * Math.cos(((2*Math.PI)/array.length)*index));
-  const coordinate = [chairOriginX, chairOriginY];
+const formation = day.formation.map((name, index, array) => {
+  const coodX = originX + ((outerCircleRadius) * Math.sin(((2*Math.PI)/array.length)*index));
+  const coodY = originY - ((outerCircleRadius) * Math.cos(((2*Math.PI)/array.length)*index));
+  const coordinate = [coodX, coodY];
   const person = {
     name,
     coordinate
   };
-  augmentedFormation.push(person);
+  return person;
 });
 
-const augmentedPasses = [];
+let order = day.passes.reduce((acc, pass, index) => {
+  acc.push(pass.from);
+  if (index === day.passes.length -1) {
+    acc.push(pass.to);
+  }
+  return acc;
+}, []);
 
-day.passes.forEach((pass, index, array) => {
-  const augmentedPass = {
-    passIndex: pass.passIndex,
-    from: {
-      name: pass.from,
-      coordinate: []
-    },
-    to: {
-      name: pass.to,
-      coordinate: []
-    }
-  };
-  augmentedFormation.forEach((person, index, array) => {
-    if (person.name === pass.from) {
-      augmentedPass.from.coordinate = person.coordinate;
-    }
-    if (person.name === pass.to) {
-      augmentedPass.to.coordinate = person.coordinate;
-    }
-  });
-  augmentedPasses.push(augmentedPass);  
+order = order.map(element => {
+  return formation.filter(person => {
+    return person.name === element;
+  })[0];
 });
-
 
 
 let lineGenerator = d3.line()
   .x(person => person.coordinate[0])
   .y(person => person.coordinate[1]);
 
-augmentedPasses.forEach((augmentedPass, index, array) => {
-  let pathData = lineGenerator([augmentedPass.from, augmentedPass.to]);
-  let line = svg.append('path')
-    .attr('d', pathData)
-    .attr('stroke', 'black')
-    .attr('fill', 'transparent');
-});
+let pathData = lineGenerator(order);
 
-let coordinates = [];
-
-augmentedPasses.forEach((augmentedPass, index, array) => {
-  const coordinate = [augmentedPass.from, augmentedPass.to];
-  coordinates.push(coordinate);
-});
-
-console.log(coordinates);
-
-
-
-  // let line = svg.append('path')
-  //   .attr('d', pathData)
-  //   .attr('stroke', 'black')
-  //   .attr('fill', 'transparent');
+let line = svg.append('path')
+  .attr('d', pathData)
+  .attr('stroke', 'black')
+  .attr('fill', 'transparent');
